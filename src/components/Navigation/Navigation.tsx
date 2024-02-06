b22b0bbfbeecb2ac294c27cc9e6775ac19731a61
Sued Nepomuceno <sued.muniz.1@gmail.com>
2024-02-06 18:25:41 -0300

    Adding anchor tag with the link to the repository on Github.

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -10,10 +10,12 @@ const Navigation = () => {
           <NavigationOption key={title} title={title} link={link} />
         ))}
       </div>
-      <button className="w-20 h-10 flex justify-around items-center rounded-2xl text-white px-2 bg-orange-500 transition easy-in-out delay-150 duration-300 hover:bg-orange-800">
-        <FaCodeFork size={20} />
-        <FaStar size={20} />
-      </button>
+      <a href="https://github.com/suedmuniz1/my-portfolio" target="_blank">
+        <button className="w-20 h-10 flex justify-around items-center rounded-2xl text-white px-2 bg-orange-500 transition easy-in-out delay-150 duration-300 hover:bg-orange-800">
+          <FaCodeFork size={20} />
+          <FaStar size={20} />
+        </button>
+      </a>
     </div>
   );
 };
