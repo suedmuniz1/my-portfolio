f96761f8fd3758ef4d18d685244e4d9afb733b40
Sued Nepomuceno <sued.muniz.1@gmail.com>
2024-02-06 14:15:40 -0300

    Removing React import.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Home from "./components/Home/Home";
