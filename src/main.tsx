c8040b8c7b6bb27385cf7871259314a63da4b5f4
Sued Nepomuceno <sued.muniz.1@gmail.com>
2024-02-06 16:02:00 -0300

    Tests for deploy.

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import "./main.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
