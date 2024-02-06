a1e9b06893840b2b76ae79a1ddb10821ed86aafe
Sued Nepomuceno <sued.muniz.1@gmail.com>
2024-02-06 16:14:13 -0300

    Removing basename prop.

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import "./main.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <HashRouter basename="/">
+    <HashRouter>
       <App />
     </HashRouter>
   </React.StrictMode>
